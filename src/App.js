09cf69b37d97cd2425059704bcae557c8c7c16ee
lindaJang <linda@plip.ai>
2017-05-04 16:19:09 +0900

    example

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Codelab extends React.Component{
+    render(){
+        return(
+            <div>Codelab</div>
+        );
+    }
+
+}
+
+class App extends React.Component{
+    render()
+    {
+        return(
+            <Codelab/>
+        );
+    }
+
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
+export default App;
